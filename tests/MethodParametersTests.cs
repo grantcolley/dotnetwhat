1877b595e77aa084cd883228bb50abe54e3b0b1b
grantcolley <grantcolley@yahoo.com>
2023-03-19 13:01:24 +0000

    Update MethodParametersTests.cs

--- tests/MethodParametersTests.cs
+++ tests/MethodParametersTests.cs
@@ -20,5 +20,26 @@ namespace dotnetwhat.tests
             Assert.AreEqual(1, foo.Value1);
             Assert.AreEqual(100, foo.Value2);
         }
+
+        [TestMethod]
+        public void Boxing_Value_Types()
+        {
+            // Arrange
+            int localInt = 5;
+
+            // Act
+            string string1 = string.Format("{0}", localInt);            // boxing
+            string string2 = string.Format("{0}", localInt.ToString()); // no boxing
+            string string3 = string.Concat("Foo", localInt);            // boxing
+            string string4 = string.Concat("Foo", localInt.ToString()); // no boxing
+            string string5 = $"{localInt}";                             // no boxing
+
+            // Assert
+            Assert.AreEqual("5", string1);
+            Assert.AreEqual("5", string2);
+            Assert.AreEqual("Foo5", string3);
+            Assert.AreEqual("Foo5", string4);
+            Assert.AreEqual("5", string5);
+        }
     }
 }
