a1a2786f3ae3b0506259d382052850c72e9d4d19
grantcolley <grantcolley@yahoo.com>
2023-03-25 18:56:55 +0000

    Create MutateString.cs

---
+++ src/MutateString.cs
@@ -0,0 +1,19 @@
+﻿namespace dotnetwhat.library
+{
+    public static class MutateString
+    {
+        public static void Mutate(string source, string target)
+        {
+            unsafe
+            {
+                fixed(char* ptr = source)
+                {
+                    for (int i = 0; i < source.Length; i++) 
+                    {
+                        ptr[i] = target[i];
+                    }
+                }
+            }
+        }
+    }
+}
