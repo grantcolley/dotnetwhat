b9bb57df5804fe66c3df63a721396ac8fbf69d49
grantcolley <grantcolley@yahoo.com>
2023-03-30 20:29:21 +0100

    Update MutateString.cs

--- src/MutateString.cs
+++ src/MutateString.cs
@@ -1,8 +1,10 @@
-﻿namespace dotnetwhat.library
+﻿using System.Runtime.InteropServices;
+
+namespace dotnetwhat.library
 {
     public static class MutateString
     {
-        public static void Mutate(string source, string target)
+        public static void Mutate_Using_Fixed(string source, string target)
         {
             unsafe
             {
@@ -13,6 +15,17 @@
                         ptr[i] = target[i];
                     }
                 }
+            }
+        }
+
+        public static void Mutate_Using_Memory_Span(string source, string target)
+        {
+            var memory = MemoryMarshal.AsMemory(source.AsMemory());
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                ref char c = ref memory.Span[i];
+                c = target[i];
             }
         }
     }
