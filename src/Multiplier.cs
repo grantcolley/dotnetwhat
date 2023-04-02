5bf286bd5041c7c873b6f6e53e290c6383cc6f73
grantcolley <grantcolley@yahoo.com>
2023-04-02 18:58:58 +0100

    Create Multiplier.cs

---
+++ src/Multiplier.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnetwhat.library
+{
+    public class Multiplier
+    {
+        public int Multiply(int value1, int value2)
+        {
+            Func<int, int, int> local = (v1, v2) => v1 * v2;
+
+            return local(value1, value2);
+        }
+    }
+}
