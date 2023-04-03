42f742e4676e3321549bda0100cebe98aa70e162
grantcolley <grantcolley@yahoo.com>
2023-04-03 12:36:23 +0100

    Create CapturedVariable.cs

---
+++ src/CapturedVariable.cs
@@ -0,0 +1,16 @@
+﻿namespace dotnetwhat.library
+{
+    public class CapturedVariable
+    {
+        public int IncrementLocalVariable()
+        {
+            int myLocalValue = 0;
+
+            Func<int> increment = () => myLocalValue++;
+
+            increment();
+
+            return myLocalValue;
+        }
+    }
+}
