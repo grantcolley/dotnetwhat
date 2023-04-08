735b40e9135564b15682f02022ad684d647cced7
grantcolley <grantcolley@yahoo.com>
2023-04-08 13:16:53 +0100

    Create Looping_While.cs

---
+++ src/Looping_While.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace dotnetwhat.library
+{
+    public class Looping_While
+    {
+        public string While()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var funcs = new List<Func<int>>(2);
+
+            int i = 0;
+
+            while(i < 2)
+            {
+                i++;
+                funcs.Add(() => i);
+            }
+
+            sb.Append(funcs[0]().ToString());
+            sb.Append(funcs[1]().ToString());
+
+            return sb.ToString();
+        }
+    }
+}
