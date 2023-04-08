435fcabc940d271e89de270407f700616055985b
grantcolley <grantcolley@yahoo.com>
2023-04-08 14:03:24 +0100

    Update Looping_Foreach.cs

--- src/Looping_Foreach.cs
+++ src/Looping_Foreach.cs
@@ -13,13 +13,13 @@ namespace dotnetwhat.library
 
             foreach (int v in vals)
             {
-                funcs.Add(() => v);
+                funcs.Add(() => v); // a fresh copy of the closed variable with each iteration
             }
 
-            sb.Append(funcs[0]().ToString());
-            sb.Append(funcs[1]().ToString());
+            sb.Append(funcs[0]().ToString()); // closed variable evaluated when delegate is invoked
+            sb.Append(funcs[1]().ToString()); // closed variable evaluated when delegate is invoked
 
-            return sb.ToString();
+            return sb.ToString(); // returns 12
         }
     }
 }
