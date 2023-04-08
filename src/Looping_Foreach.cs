fd1c38b5686db18454bd26997884908621d37c68
grantcolley <grantcolley@yahoo.com>
2023-04-08 13:16:50 +0100

    Create Looping_Foreach.cs

---
+++ src/Looping_Foreach.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace dotnetwhat.library
+{
+    public class Looping_Foreach
+    {
+        public string ForEach()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var vals = new List<int> { 1, 2 };
+            var funcs = new List<Func<int>>();
+
+            foreach (int v in vals)
+            {
+                funcs.Add(() => v);
+            }
+
+            sb.Append(funcs[0]().ToString());
+            sb.Append(funcs[1]().ToString());
+
+            return sb.ToString();
+        }
+    }
+}
