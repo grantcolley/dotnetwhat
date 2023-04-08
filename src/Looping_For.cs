2fbc5c964bf893d1bafc83495ca0f655bb2349c0
grantcolley <grantcolley@yahoo.com>
2023-04-08 13:16:48 +0100

    Create Looping_For.cs

---
+++ src/Looping_For.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace dotnetwhat.library
+{
+    public class Looping_For
+    {
+        public string For()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var funcs = new List<Func<int>>(2);
+
+            for(int i = 0; i < 2; i++)
+            {
+                funcs.Add(() => i);
+            }
+
+            sb.Append(funcs[0]().ToString());
+            sb.Append(funcs[1]().ToString());
+
+            return sb.ToString();
+        }
+    }
+}
