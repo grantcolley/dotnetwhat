f10aa51575c377a270e84e82fca88ff91260cefc
grantcolley <grantcolley@yahoo.com>
2023-07-10 16:13:31 +0100

    Create Threads.cs

---
+++ src/Threads.cs
@@ -0,0 +1,19 @@
+﻿namespace dotnetwhat.library
+{
+    public class Threads
+    {
+        public void RunThread()
+        {
+            var message = "Hello World!";
+
+            var thread = new Thread(WriteToConsole);
+            thread.IsBackground = true;
+            thread.Start(message);            
+        }
+
+        private static void WriteToConsole(object stateInfo)
+        {
+            Console.WriteLine(stateInfo);
+        }
+    }
+}
