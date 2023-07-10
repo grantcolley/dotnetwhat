1f3cecbbe067f0af01aea112efd19ee1683173cc
grantcolley <grantcolley@yahoo.com>
2023-07-10 16:13:06 +0100

    Create NullableValueTypes.cs

---
+++ src/NullableValueTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetwhat.library
+{
+    public class NullableValueTypes
+    {
+        public void Test123()
+        {
+            int i = 5;
+            int? j = null;
+            j = 7;
+            _ = j.HasValue;
+            _ = j.Value;
+        }
+    }
+}
