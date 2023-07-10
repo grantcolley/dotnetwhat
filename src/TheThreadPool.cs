346474bf8fd5520e2f96220d59e24f3e1481ed9c
grantcolley <grantcolley@yahoo.com>
2023-07-10 16:13:26 +0100

    Create TheThreadPool.cs

---
+++ src/TheThreadPool.cs
@@ -0,0 +1,17 @@
+﻿namespace dotnetwhat.library
+{
+    public class TheThreadPool
+    {
+        public void RunThreadFromThreadPool()
+        {
+            var message = "Hello World!";
+
+            ThreadPool.QueueUserWorkItem(WriteToConsole, message);
+        }
+
+        private static void WriteToConsole(object stateInfo)
+        {
+            Console.WriteLine(stateInfo);
+        }
+    }
+}
