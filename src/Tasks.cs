3625508b57eb1c284b4afa96fae8a975c2db2fda
grantcolley <grantcolley@yahoo.com>
2023-07-10 16:13:20 +0100

    Create Tasks.cs

---
+++ src/Tasks.cs
@@ -0,0 +1,21 @@
+﻿namespace dotnetwhat.library
+{
+    public class Tasks
+    {
+        public void RunTask()
+        {
+            var message = "Hello World!";
+
+            _ = Task.Run(() => WriteToConsole(message));
+
+            // this does the same thing as Task.Run()
+            _ = Task.Factory.StartNew(() => WriteToConsole(message),
+                    CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+        }
+
+        private static void WriteToConsole(string stateInfo)
+        {
+            Console.WriteLine(stateInfo);
+        }
+    }
+}
